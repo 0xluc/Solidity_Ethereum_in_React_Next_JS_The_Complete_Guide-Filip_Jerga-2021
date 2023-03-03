c75b4a4c2af67ea1010c72054fb756f812b52d97
Lucas Mourão <lucasbrodrigues14@gmail.com>
2023-03-03 00:01:05 -0300

    added funders and getAllFunders()

--- faucet/contracts/faucetContract.sol
+++ faucet/contracts/faucetContract.sol
@@ -1,12 +1,19 @@
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity >=0.8.4 <0.9.0;
 
 contract Faucet {
+
+
+    address[] public funders;
+
     // this function is called when no function name is specified in the call
     // external functions can only be called by other contracts and other txs
     receive() external payable { // payable tell this function will receive tokens
 
     }
     function addFunds() external payable {
-        
+        funders.push(msg.sender);
+    }
+    function getAllFunders() external view returns(address[] memory){
+        return funders;
     }
 }
