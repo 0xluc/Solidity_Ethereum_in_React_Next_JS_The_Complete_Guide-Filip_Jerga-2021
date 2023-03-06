74e9fd04f993cbebc4736f69007e72e014434d8a
Lucas Mourão <lucasbrodrigues14@gmail.com>
2023-03-05 22:37:44 -0300

    ui creation and metamask integration

--- faucet/src/App.js
+++ faucet/src/App.js
@@ -1,7 +1,30 @@
+import { useEffect } from "react";
+import "./App.css"
+
 function App() {
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      
+    }
+    loadProvider()
+  },[])
+
   return (
-    <div className="App">
-     Hello world 
+    <div className="faucet-wrapper">
+      <div className="faucet">
+        <div className="balance-view is-size-2">
+          Current Balanace: <strong>10</strong>ETH
+        </div>
+        <button className="btn mr-2" onClick={async () => {
+          const accounts = await window.ethereum.request({method:"eth_requestAccounts"})
+          console.log(accounts)
+        } }>
+          Enable ethereum
+        </button>
+        <button className="btn mr-2">Donate</button>
+        <button className="btn">Withdraw</button>
+      </div>
     </div>
   );
 }
