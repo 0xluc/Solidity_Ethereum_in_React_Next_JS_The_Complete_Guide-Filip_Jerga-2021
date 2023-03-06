de3ca8ee027377836616c9710de959f6a8f4a93c
Lucas Mourão <lucasbrodrigues14@gmail.com>
2023-03-06 07:11:25 -0300

    metamask connection and display connected account address

--- faucet/src/App.js
+++ faucet/src/App.js
@@ -1,27 +1,75 @@
-import { useEffect } from "react";
-import "./App.css"
+import { useEffect, useState } from "react";
+import "./App.css";
+import Web3 from "web3";
+import detectEthereumProvider from "@metamask/detect-provider";
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({ provider: null, web3: null });
+
+  const [account, setAccounts] = useState(null);
 
   useEffect(() => {
     const loadProvider = async () => {
+      const provider = await detectEthereumProvider()
+      if(provider){
+        await provider.request({ method: "eth_requestAccounts" })
+        setWeb3Api({
+          web3: new Web3(provider),
+          provider,
+        });
+      } else {
+        console.error("Please install metamask")
+      }
       
-    }
-    loadProvider()
-  },[])
+      // if (window.ethereum) {
+      //   provider = window.ethereum;
+
+      //   try {
+      //     await provider.request({ method: "eth_requestAccounts" });
+      //   } catch {
+      //     console.error("User denied account access!");
+      //   }
+      // } else if (window.web3) {
+      //   provider = window.web3.currentProvider;
+      // } else if (!process.env.production) {
+      //   provider = new Web3.providers.HttpProvider(
+      //     "https://api.avax-test.network/ext/bc/c/rpc"
+      //   );
+      // }
+
+    };
+    loadProvider();
+  }, []);
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts();
+      setAccounts(accounts[0]);
+    };
+    web3Api.web3 && getAccount();
+  }, [web3Api.web3]);
 
   return (
     <div className="faucet-wrapper">
       <div className="faucet">
+        <span>
+          <strong>Account:</strong>
+          <h1>{account ? account : "Not connected"}</h1>
+        </span>
         <div className="balance-view is-size-2">
           Current Balanace: <strong>10</strong>ETH
         </div>
-        <button className="btn mr-2" onClick={async () => {
-          const accounts = await window.ethereum.request({method:"eth_requestAccounts"})
-          console.log(accounts)
-        } }>
+        {/* <button
+          className="btn mr-2"
+          onClick={async () => {
+            const accounts = await window.ethereum.request({
+              method: "eth_requestAccounts",
+            });
+            console.log(accounts);
+          }}
+        >
           Enable ethereum
-        </button>
+        </button> */}
         <button className="btn mr-2">Donate</button>
         <button className="btn">Withdraw</button>
       </div>
