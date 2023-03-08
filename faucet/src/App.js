5155c69d19b3098d2fcaa253ee8fcfb32b70074d
Lucas Mourão <lucasbrodrigues14@gmail.com>
2023-03-08 00:00:18 -0300

    contract artifact integration

--- faucet/src/App.js
+++ faucet/src/App.js
@@ -2,25 +2,33 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Web3 from "web3";
 import detectEthereumProvider from "@metamask/detect-provider";
+import { loadContract } from "./utils/load-contract";
 
 function App() {
-  const [web3Api, setWeb3Api] = useState({ provider: null, web3: null });
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+    contract: null,
+  });
 
+  const [balance, setBalance] = useState(null);
   const [account, setAccounts] = useState(null);
 
   useEffect(() => {
     const loadProvider = async () => {
-      const provider = await detectEthereumProvider()
-      if(provider){
-        await provider.request({ method: "eth_requestAccounts" })
+      const provider = await detectEthereumProvider();
+      const contract = await loadContract("Faucet", provider);
+      if (provider) {
+        //await provider.request({ method: "eth_requestAccounts" })
         setWeb3Api({
           web3: new Web3(provider),
           provider,
+          contract,
         });
       } else {
-        console.error("Please install metamask")
+        console.error("Please install metamask");
       }
-      
+
       // if (window.ethereum) {
       //   provider = window.ethereum;
 
@@ -36,10 +44,18 @@ function App() {
       //     "https://api.avax-test.network/ext/bc/c/rpc"
       //   );
       // }
-
     };
     loadProvider();
   }, []);
+
+  useEffect(() => {
+    const loadBalance = async () => {
+      const { contract, web3 } = web3Api;
+      const balance = await web3.eth.getBalance(contract.address);
+      setBalance(web3.utils.fromWei(balance, "ether"));
+    };
+    web3Api.contract && loadBalance();
+  }, [web3Api]);
 
   useEffect(() => {
     const getAccount = async () => {
@@ -52,12 +68,25 @@ function App() {
   return (
     <div className="faucet-wrapper">
       <div className="faucet">
-        <span>
-          <strong>Account:</strong>
-          <h1>{account ? account : "Not connected"}</h1>
-        </span>
-        <div className="balance-view is-size-2">
-          Current Balanace: <strong>10</strong>ETH
+        <div className="is-flex is-align-items-center">
+          <span>
+            <strong className="mr-2">Account:</strong>
+          </span>
+          {account ? (
+            <div>{account}</div>
+          ) : (
+            <button
+              onClick={() =>
+                web3Api.provider.request({ method: "eth_requestAccounts" })
+              }
+              className="button is-info is-small"
+            >
+              Connect Wallet
+            </button>
+          )}
+        </div>
+        <div className="balance-view is-size-2 my-4">
+          Current Balanace: <strong>{balance}</strong>ETH
         </div>
         {/* <button
           className="btn mr-2"
@@ -70,8 +99,8 @@ function App() {
         >
           Enable ethereum
         </button> */}
-        <button className="btn mr-2">Donate</button>
-        <button className="btn">Withdraw</button>
+        <button className="button is-link mr-2">Donate</button>
+        <button className="button is-primary is-light">Withdraw</button>
       </div>
     </div>
   );
