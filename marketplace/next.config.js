6148e3029f726137b267aa1b2ad28a6a56c40d36
Lucas Mourão <lucasbrodrigues14@gmail.com>
2023-03-12 22:07:04 -0300

    base project

---
+++ marketplace/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "thrangra.sirv.com"
+    ]
+  }
+}
+
+module.exports = nextConfig
